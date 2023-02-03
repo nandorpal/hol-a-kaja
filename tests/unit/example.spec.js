8b4cdbe4623560616b2e3666ef5b0a4bd6685b8a
Nandor Pal <nandor.pal@acetech.dev>
2023-02-03 17:28:36 +0200

    fix: example unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/Ask.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
+describe('App.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(App, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
